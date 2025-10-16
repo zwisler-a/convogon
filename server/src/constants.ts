891a1742717169f3e84c82e4d7c322223dc05471
hetjiz <hetjiz@proton.me>
2025-10-16 09:09:45 +0200

    Fix: responsiveness

--- server/src/constants.ts
+++ server/src/constants.ts
@@ -4,7 +4,7 @@ export const jwtSecret = process.env.JWT_SECRET ?? "local"
 export const mailTemplate = (link: string)=> `
 <h1>Hey :)</h1>
 <p>
-Um die Registrierung abzuschließen oder dich anzumelden, klicke bitte auf den folgenden Link: <a href="${link}">Registrierung abschließen</a>
+Um die Registrierung abzuschließen oder dich anzumelden, klicke bitte auf diesen <a href="${link}">Link</a>
 </p>
 <p>
 <br/>
