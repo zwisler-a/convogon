e7c0fc34f11d19853aee98e68e222fbe61b24107
hetjiz <hetjiz@proton.me>
2025-10-19 11:29:05 +0200

    Feat: translations

--- client/src/app/shared/persona-overview/persona-overview.ts
+++ client/src/app/shared/persona-overview/persona-overview.ts
@@ -1,16 +1,39 @@
-import {Component, Input} from '@angular/core';
-import {DatePipe, KeyValuePipe} from "@angular/common";
-import {ReactiveFormsModule} from '@angular/forms';
+import { Component, Input } from '@angular/core';
+import { DatePipe, KeyValuePipe } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-persona-overview',
-  imports: [
-    DatePipe,
-    ReactiveFormsModule
-  ],
+  imports: [DatePipe, ReactiveFormsModule],
   templateUrl: './persona-overview.html',
-  styleUrl: './persona-overview.css'
+  styleUrl: './persona-overview.css',
 })
 export class PersonaOverview {
   @Input() persona: any;
+
+  getDietDisplayString(diet: any): string {
+    if (diet == 'nothing') {
+      return 'Keine Besonderheiten';
+    } else if (diet == 'vegetarian') {
+      return 'Vegetarisch';
+    } else if (diet == 'vegan') {
+      return 'Vegan';
+    } else if (diet == 'other') {
+      return this.persona.dietOther;
+    } else {
+      return 'Unbekannt';
+    }
+  }
+
+  getAccommodationDisplayString(accommodation: any): string {
+    if (accommodation == 'hut') {
+      return 'Hütte';
+    } else if (accommodation == 'tent') {
+      return 'Mein Zelt / Van';
+    } else if (accommodation == 'other') {
+      return 'Ich übernachte außerhalb';
+    } else {
+      return 'Unbekannt';
+    }
+  }
 }
