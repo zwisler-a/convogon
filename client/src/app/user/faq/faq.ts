a5ccd27e1af67d20820419503e4111f7af947dfc
zwisler <alex-z-@web.de>
2025-10-23 19:20:43 +0200

    feat: faq style

--- client/src/app/user/faq/faq.ts
+++ client/src/app/user/faq/faq.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { MatButton } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { RouterLink } from '@angular/router';
+import {MatCard, MatCardContent} from '@angular/material/card';
 
 @Component({
   selector: 'app-faq',
-  imports: [RouterLink, MatIconModule, MatButton],
+  imports: [RouterLink, MatIconModule, MatButton, MatCard, MatCardContent],
   templateUrl: './faq.html',
   styleUrl: './faq.css',
 })
