540e6d3238e6d1b6d8e9e6406f0fbb7d6336d3a3
hetjiz <hetjiz@proton.me>
2025-11-05 11:15:31 +0100

    Fix: Small adjustments II

--- client/src/app/user/pay/pay.ts
+++ client/src/app/user/pay/pay.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { ROUTES } from '../../app.routes';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-pay',
-  imports: [],
+  imports: [RouterLink, MatButtonModule, MatIconModule],
   templateUrl: './pay.html',
-  styleUrl: './pay.css'
+  styleUrl: './pay.css',
 })
 export class Pay {
-
+  protected readonly ROUTES = ROUTES;
 }
